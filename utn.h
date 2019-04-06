2065775320799101844f53a0202355d0e6dffc23
nicolasalvarez117 <47337963+nicolasalvarez117@users.noreply.github.com>
2019-04-05 22:23:33 -0300

    Delete utn.h

--- utn.h
+++
@@ -1,8 +0,0 @@
-
-int add(int numeroUno, int numeroDos);
-int multiply(int numeroUno, int numeroDos);
-int subtract(int numeroUno, int numeroDos);
-float divide(float numeroUno, float numeroDos);
-int factorial(int numeroUno);
-
-
